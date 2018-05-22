fd266359867f9bbe03ef4b8ee130959f1e6638fe
tyarosevich <tyranosevich@gmail.com>
2018-05-22 15:19:30 -0700

    part 1 done

--- HW/HW13/yarosevich_hw_13_rootfinder.m
+++ HW/HW13/yarosevich_hw_13_rootfinder.m
@@ -2,7 +2,7 @@ clc; clear all; close all;
 % Root finder for Part 1
 x_mesh = linspace(-1.8, 1.8, 100);
 x0_list = zeros(1, length(x_mesh));
-t = .6;
+t = 2.5;
 %fun1 = @(x0, x) (x - x0) .* exp(x0.^2);
 
 for i = 1:length(x_mesh)
@@ -11,9 +11,18 @@ fun = @(x0) x0 - x + t * exp(-x0^2);
 x0_list(i) = fzero(fun, x);
 
 end
-
 u = exp(-x0_list.^2);
-%plot(x_mesh, u)
+figure
+hold on
+plot(x_mesh, u)
+set(gca, 'FontSize', [12], 'FontName', 'Times')
+set(gcf,'color','w');
+xlabel('x', 'FontSize', 15)
+ylabel('u(x,2)', 'FontSize', 15)
+axis([-1.8 1.8 0 1])
+
+hold off
+export_fig hw_13_plot7.pdf
 %% Characteristic Lines for Part 1
 figure
 hold on
@@ -24,8 +33,13 @@ for x0 = linspace(-2, 2, 50)
     xlabel('x')
     ylabel('t')
 end
-axis([-1.8 1.8 0 2])
+    axis([-1.8 1.8 0 2])
+set(gca, 'FontSize', [12], 'FontName', 'Times')
+set(gcf,'color','w');
+xlabel('x', 'FontSize', 15)
+ylabel('t', 'FontSize', 15)
 hold off
+export_fig hw_13_plot1.pdf
 
 %% Characteristic Lines for Part 2
 clc; clear all; close all;
