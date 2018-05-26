68a21f0880e30a8cd7637814c48627e8e11d5135
tyarosevich <tyranosevich@gmail.com>
2018-05-26 14:39:19 -0700

    random commit

--- HW/HW14/yarosevich_hw14_plots.m
+++ HW/HW14/yarosevich_hw14_plots.m
@@ -1,6 +1,6 @@
 clc; clear all; close all;
 
-x_mesh = linspace(-2*pi, 2*pi, 100);
+x_mesh = linspace(-2*pi, 2*pi, 1000);
 % x_mesh_R = x_mesh_R(2:end);
 % x_mesh_L = linspace(-2*pi, 1,100);
 figure
@@ -9,12 +9,12 @@ for x0 = linspace(-2*pi, 2*pi, 100)
     if x0 < 1
         t = x_mesh - x0;
     elseif x0 > 1
-        t = (2/sqrt(2)) * (x_mesh - x0);
+        t = sqrt(2) * (x_mesh - x0);
     end
     plot(x_mesh, t, 'Color', 'b')
 
 end
-t_s = ((x_mesh - 1) * pi) / (sqrt(2));
+t_s = ((x_mesh - 1) * pi) / (2 * sqrt(2));
 plot(x_mesh, t_s, 'r')
 
 xlabel('x')
@@ -23,4 +23,31 @@ set(gca, 'FontSize', [12], 'FontName', 'Times')
 set(gcf,'color','w');
 xlabel('x', 'FontSize', 15)
 ylabel('t', 'FontSize', 15)
-axis([-pi pi 0 4])
+axis([0 4 0 4])
+%%
+clc; clear all; close all;
+
+x_mesh = linspace(-2*pi, 2*pi, 1000);
+% x_mesh_R = x_mesh_R(2:end);
+% x_mesh_L = linspace(-2*pi, 1,100);
+figure
+hold on
+for x0 = linspace(-2*pi, 2*pi, 100)
+    if x0 > 1
+        t = x_mesh - x0;
+    elseif x0 < 1
+        t = (2/sqrt(2)) * (x_mesh - x0);
+    end
+    plot(x_mesh, t, 'Color', 'b')
+
+end
+% t_s = ((x_mesh - 1) * pi) / (2 * sqrt(2));
+% plot(x_mesh, t_s, 'r')
+
+xlabel('x')
+ylabel('t')
+set(gca, 'FontSize', [12], 'FontName', 'Times')
+set(gcf,'color','w');
+xlabel('x', 'FontSize', 15)
+ylabel('t', 'FontSize', 15)
+axis([0 4 0 4])
